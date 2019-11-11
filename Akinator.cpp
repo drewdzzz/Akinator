9022d29348da1557c311254291d207a8cd44cc0b
drew- <Andrey22102001@gmail.com>
2019-11-11 13:16:13 +0300

    Changes in description
    1.Now bin-tree is protected instead of private.
    2. Now you can write your tree in file

--- Akinator.cpp
+++ Akinator.cpp
@@ -2,6 +2,8 @@
 #include <cassert>
 static const int BUFSIZE = 256;
 
+char userInput[BUFSIZE] = "";
+
 class Akinator_tree : public Tree_t<char*>
 {
     char symb = 0;
@@ -76,12 +78,74 @@ public:
         read_undertree (stream, head);
         fclose (stream);
     }
+
+    void write_undertree (FILE* stream, Node_t *node)
+    {
+        assert (stream);
+        assert (node);
+
+        fprintf (stream, "{ '");
+        write_data(stream, node -> data);
+        fprintf (stream, "' ");
+        if (node -> left)
+            write_undertree (stream, node -> left);
+        if (node -> right)
+            write_undertree (stream, node -> right);
+        fprintf (stream, "}");
+    }
+
+    friend void go_lower_and_ask (Akinator_tree tree, Akinator_tree::Node_t *node);
 };
+
+
+void go_lower_and_ask (Akinator_tree tree, Akinator_tree::Node_t *node)
+{
+    if ( node -> right && node -> left )
+    {
+        tree.write_data (stdout, node -> data);
+        printf ("?\n");
+        scanf ("%s", userInput);
+        if ( ! strcmp ("да", userInput) || ! strcmp ("yes", userInput) )
+            go_lower_and_ask (tree, node -> right);
+        else if ( ! strcmp ("нет", userInput) || ! strcmp ("no", userInput) )
+            go_lower_and_ask (tree, node -> left);
+        else
+        {
+            fprintf (stderr, "Wrong answer, try again\n");
+            go_lower_and_ask (tree, node);
+        }
+    }
+    else
+        printf ("%s", node -> data);
+}
+
+void game_logic (Akinator_tree tree)
+{
+    printf ("Здравствуй, мой друг. Сыграем в акинатора? Предупреждаю, обыграть меня ооочень непростоб хе-хе;)\n");
+    scanf ("%s", userInput);
+    if ( ! strcmp ("да", userInput) || ! strcmp ("yes", userInput) )
+        go_lower_and_ask (tree, tree.head);
+    else if ( ! strcmp ("нет", userInput) || ! strcmp ("no", userInput) )
+        return;
+    else
+    {
+        fprintf (stderr, "Wrong answer, try again\n");
+        game_logic(tree);
+    }
+}
+
 
 int main ()
 {
+    setlocale (LC_ALL, "rus");
     Akinator_tree tree;
     tree.read_tree((char*)"tree-base.txt");
     tree.draw ((char*)"open");
+    FILE* stream = fopen ("out.txt", "w");
+    tree.write_undertree (stream, tree.head);
+
+    game_logic (tree);
+
+
     return 0;
 }

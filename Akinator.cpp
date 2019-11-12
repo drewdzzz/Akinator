24139b473bea40b344496fbc9e8fe0d79b26d20c
drew- <Andrey22102001@gmail.com>
2019-11-12 03:30:44 +0300

    Update
    1.Updated tree writing
    2.Added function of addingg a new character

--- Akinator.cpp
+++ Akinator.cpp
@@ -2,7 +2,7 @@
 #include <cassert>
 static const int BUFSIZE = 256;
 
-char userInput[BUFSIZE] = "";
+static char userInput[BUFSIZE] = "";
 
 class Akinator_tree : public Tree_t<char*>
 {
@@ -10,6 +10,7 @@ class Akinator_tree : public Tree_t<char*>
     int read_count = 0;
     char buf[BUFSIZE] = "";
     bool YES_flag = false;
+
     void read_undertree (FILE* stream, Node_t *node)
     {
         assert (stream);
@@ -43,6 +44,21 @@ class Akinator_tree : public Tree_t<char*>
             default : fprintf (stderr, "Wrong syntax. Waited for '{' or '}'"); abort(); break;
         }
 
+    }
+
+    void write_undertree (FILE* stream, Node_t *node)
+    {
+        assert (stream);
+        assert (node);
+
+        fprintf (stream, "{ '");
+        write_data(stream, node -> data);
+        fprintf (stream, "' ");
+        if (node -> left)
+            write_undertree (stream, node -> left);
+        if (node -> right)
+            write_undertree (stream, node -> right);
+        fprintf (stream, "}");
     }
 
     void draw_nodes (FILE* stream, Node_t *node) override
@@ -79,58 +95,113 @@ public:
         fclose (stream);
     }
 
-    void write_undertree (FILE* stream, Node_t *node)
+    void write_tree (char* output_file)
     {
+        FILE* stream = fopen (output_file, "w");
         assert (stream);
-        assert (node);
-
-        fprintf (stream, "{ '");
-        write_data(stream, node -> data);
-        fprintf (stream, "' ");
-        if (node -> left)
-            write_undertree (stream, node -> left);
-        if (node -> right)
-            write_undertree (stream, node -> right);
-        fprintf (stream, "}");
+        write_undertree (stream, head);
+        fclose (stream);
     }
-
     friend void go_lower_and_ask (Akinator_tree &tree, Akinator_tree::Node_t *node);
+    friend void add_character (Akinator_tree &tree, Akinator_tree::Node_t *node);
 };
+
+bool check_answer ()
+{
+    printf ("\nЯ угадал?\n");
+    {
+        scanf ("%s", userInput);
+
+        if ( ! strcmp ("да", userInput) || ! strcmp ("yes", userInput) )
+            return true;
+        else if ( ! strcmp ("нет", userInput) || ! strcmp ("no", userInput) )
+            return false;
+        else
+            printf ("Неправильный ответ, попробуй ещё\n");
+    }
+}
+
+void add_character (Akinator_tree &tree, Akinator_tree::Node_t *node)
+{
+    int read_count;
+    printf (":(\n"
+            "Хорошо, кто это был?\n");
+    scanf (" %[^\n]%n%", userInput, &read_count );
+    tree.make_right (node, nullptr);
+    node -> right -> data = (char*) calloc ( tree.read_count + 1, sizeof (char) );
+    strcpy (node -> right -> data, userInput);
+
+    tree.make_left (node, nullptr);
+    node -> left -> data = (char*) calloc ( tree.read_count + 1, sizeof (char) );
+    strcpy (node -> left -> data, node -> data);
+
+    printf ("И чем же ваш персонаж отличается от моего?\n");
+    scanf (" %[^\n]%n%", userInput, &read_count );
+    node -> data = (char*)realloc (node -> data, (tree.read_count + 1) * sizeof (char) );
+    node -> data = userInput;
+}
 
 
 void go_lower_and_ask (Akinator_tree &tree, Akinator_tree::Node_t *node)
 {
     if ( node -> right && node -> left )
     {
+        printf ("\033[1;36m");
         tree.write_data (stdout, node -> data);
         printf ("?\n");
-        scanf ("%s", userInput);
-        if ( ! strcmp ("да", userInput) || ! strcmp ("yes", userInput) )
-            go_lower_and_ask (tree, node -> right);
-        else if ( ! strcmp ("нет", userInput) || ! strcmp ("no", userInput) )
-            go_lower_and_ask (tree, node -> left);
-        else
+        printf ("\033[0m");
+        while (1)
         {
-            fprintf (stderr, "Wrong answer, try again\n");
-            go_lower_and_ask (tree, node);
+            scanf ("%s", userInput);
+
+            if ( ! strcmp ("да", userInput) || ! strcmp ("yes", userInput) )
+            {
+                go_lower_and_ask (tree, node -> right);
+                break;
+            }
+            else if ( ! strcmp ("нет", userInput) || ! strcmp ("no", userInput) )
+            {
+                go_lower_and_ask (tree, node -> left);
+                break;
+            }
+            else
+            {
+                printf ("Неправильный ответ, попробуй ещё\n");
+            }
         }
     }
     else
+    {
+        printf ("\033[1;32m");
         printf ("%s", node -> data);
+        printf ("\033[0m");
+        if ( ! check_answer() )
+        {
+            add_character (tree, node);
+        }
+    }
 }
 
 void game_logic (Akinator_tree &tree)
 {
-    printf ("Здравствуй, мой друг. Сыграем в акинатора? Предупреждаю, обыграть меня ооочень непростоб хе-хе;)\n");
-    scanf ("%s", userInput);
-    if ( ! strcmp ("да", userInput) || ! strcmp ("yes", userInput) )
-        go_lower_and_ask (tree, tree.head);
-    else if ( ! strcmp ("нет", userInput) || ! strcmp ("no", userInput) )
-        return;
-    else
+    printf ("Здравствуй, мой друг. Сыграем в акинатора?\n");
+
+    while (1)
     {
-        fprintf (stderr, "Wrong answer, try again\n");
-        game_logic(tree);
+        scanf ("%s", userInput);
+        if ( ! strcmp ("да", userInput) || ! strcmp ("yes", userInput) )
+        {
+            go_lower_and_ask (tree, tree.head);
+            break;
+        }
+        else if ( ! strcmp ("нет", userInput) || ! strcmp ("no", userInput) )
+        {
+            return;
+        }
+        else
+        {
+            printf ("Неправильный ответ, попробуй ещё\n");
+        }
     }
 }
 
@@ -140,12 +211,9 @@ int main ()
     setlocale (LC_ALL, "rus");
     Akinator_tree tree;
     tree.read_tree((char*)"tree-base.txt");
-    tree.draw ((char*)"open");
-    FILE* stream = fopen ("out.txt", "w");
     game_logic (tree);
-    tree.write_undertree (stream, tree.head);
-
-
+    tree.draw ((char*)"open");
+    tree.write_tree((char*)"tree-base.txt");
 
     return 0;
 }
